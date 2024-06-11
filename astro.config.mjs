2644e35e8dc4887095326d08319ec7ca92c92f03
Denis Gudiña Nuñez <syrewolfdigital@gmail.com>
2024-06-11 16:10:46 +0200

    Add langs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import tailwind from "@astrojs/tailwind"
-import robotsTxt from "astro-robots-txt"
-import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind";
+import astroI18next from "astro-i18next";
+import robotsTxt from "astro-robots-txt";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+  integrations: [astroI18next(), tailwind(), robotsTxt()],
+  site: 'https://denis-gudina.netlify.app/'
+});
